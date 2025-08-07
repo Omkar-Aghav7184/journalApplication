ca1281ab17673e73b1bbb37f10c198fc0a094b87
Omkar <omkar.t.aghav@gmail.com>
2025-08-08 00:16:22 +0530

    APITesting

--- src/main/java/net/engineeringdigest/journalApp/APITesting/MyClass.java
+++ src/main/java/net/engineeringdigest/journalApp/APITesting/MyClass.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class MyClass {
 
     @GetMapping("/omkarAghav")
