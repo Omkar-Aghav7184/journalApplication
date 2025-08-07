23e754383a49ec3696a093353725a2ef76047f63
Omkar <omkar.t.aghav@gmail.com>
2025-08-08 00:27:16 +0530

    adds JournalApplicationMain

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -8,23 +8,41 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling
+@EnableKafka
 public class JournalApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(JournalApplication.class, args);
         ConfigurableEnvironment environment = context.getEnvironment();
         System.out.println(environment.getActiveProfiles()[0]);
+
+        String redisHost = environment.getProperty("spring.data.redis.host");
+        String redisPort = environment.getProperty("spring.data.redis.port");
+
+        System.out.println("🔍 Redis Connected Host: " + redisHost + ", Port: " + redisPort);
     }
 
     @Bean
     public PlatformTransactionManager transaction(MongoDatabaseFactory dbFactory)
     {
         return new MongoTransactionManager(dbFactory);
+    }
+
+    @Bean
+    public RestTemplate restTemplate()
+    {
+        return  new RestTemplate();
     }
 }
 //PlatformTransactionManager is an Interface
